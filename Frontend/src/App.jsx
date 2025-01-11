619921ffc6ce31ec320a53bd12f78b6fa33288fc
Prasad Sunil Dinnapurkar <122858378+Dinnapurkarprasad@users.noreply.github.com>
2025-01-11 20:55:09 +0530

    Add files via upload

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import CardTemplates from './components/CardTemplates'
+import CardTemplateTwoColumn from './components/CardTempletTwoColumn'
+import AccentImage from './components/AccentImage'
+
+
+const App = () => {
+  return (
+    <div>
+      <CardTemplates/>
+    </div>
+  )
+}
+
+export default App
